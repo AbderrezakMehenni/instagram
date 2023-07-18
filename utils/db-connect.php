aea4c8a3d2f127ffdb1c13cf21d2321ddb9c408a
VinceS42 <v.fayon.pro@gmail.com>
2023-07-18 11:46:42 +0200

    modif process connexion

--- utils/db-connect.php
+++ utils/db-connect.php
@@ -1,6 +1,6 @@
 <?php
 
-    $env = parse_ini_file('../.env');
+    $env = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/.env'); // DOCUMENT_ROOT va chercher la racine sous laquelle le script courant est exécuté, comme défini dans la configuration du serveur.
 
     $address = $env['LOCAL_ADDRESS'];
 
